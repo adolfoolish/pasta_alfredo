333e051e965fbed79dc0a0d3378e2502235ed199
adolfoolish <64382861+adolfoolish@users.noreply.github.com>
2020-12-08 09:25:21 -0600

    Add files via upload

---
+++ HashAcquire.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.synopsis
+Obtiene el hash junto con el nombre de los archivos
+
+- El usuario especifica el equipo objetivo
+- El usuario especifica el directorio objetivo
+
+Este script creara un archivo que contiene
+SHA-256Hash y FilePath
+
+.Description
+Este script obtiene el hash junto con el nombre de los archivos
+
+.parameter TargetFolder
+La carpeta de donde se extraeran los hash
+
+.parameter ResultFile
+El archivo donde se creara el documento
+
+.example
+
+HashAcquire 
+Obtiene el hash junto con el nombre de los archivos
+#>
+
+
+# Parameter Definition Section
+param(  
+    [string]$TargetFolder="C:/Windows/System32/drivers/",
+    [string]$ResultFile="baseline.txt"
+)
+
+
+Get-ChildItem $TargetFolder | Get-FileHash | Select-Object -Property Hash, Path | Format-Table -HideTableHeaders | Out-File $ResultFile -Encoding ascii
+
